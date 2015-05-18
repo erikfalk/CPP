6809b437f0e4661b9ebf34ccb7c62e718b461a2b
erikfalk <erik-falk@web.de>
2015-05-18 12:25:59 +0200

    Initial Commit

--- Labor/Labor2/Labor2_Aufgabe3a/Labor2_Aufgabe3a/main.c
+++ Labor/Labor2/Labor2_Aufgabe3a/Labor2_Aufgabe3a/main.c
@@ -6,10 +6,49 @@
 //  Copyright (c) 2015 Erik Falk. All rights reserved.
 //
 
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    double *pZahl = NULL, maximum = 0, summe = 0;
+    int elemente, position = 0, i;
+    
+    printf("Wiviele Elemente wollen sie erzeugen?\n");
+    scanf("%d",&elemente);
+    
+    //Speicherresevierung
+    pZahl = (double *)malloc(elemente*sizeof(double));
+    
+    //Eingabe
+    for (i=0; i<elemente; i++){
+        printf("%d. Zahl eingeben:  ", i+1);
+        scanf("%lf", pZahl+i);
+    }
+    
+    //Verarbeitung
+    for(i=0; i<elemente; i++){
+        //Summe
+        summe +=*(pZahl+i);
+        
+        //Maximum und Position des Maximums
+        if (maximum == 0){
+            maximum = *(pZahl+i);
+            position = i+1;
+        }
+        else if (maximum <= *pZahl+i){
+            maximum = *(pZahl+i);
+            position = i+1;
+        }
+        
+    }
+    
+    //Ausgabe
+    printf("Summe: %.2lf, Maximum %.2lf, Position %d\n", summe, maximum, position);
+    
+    //Speicherfreigabe
+    free(pZahl);
+    
     return 0;
 }
---
+++ Labor/Labor2/Labor2_Aufgabe3b/Labor2_Aufgabe3b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Labor2_Aufgabe3b
+//
+//  Created by Erik Falk on 18.05.15.
+//  Copyright (c) 2015 Erik Falk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
